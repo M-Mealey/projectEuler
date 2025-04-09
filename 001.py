959044041c6ddf155ee419c36c4e49c6f537d255
Megan Mealey <mealeymn@gmail.com>
2025-04-09 13:36:57 -0500

    first 3

---
+++ 001.py
@@ -0,0 +1,26 @@
+"""
+Project Euler Problem 1
+=======================
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5,
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+
+"""
+Computes sum of arithmetic series from 0 to a, interval of x
+"""
+def arith_sum(a, x):
+    n = a//x
+    sum = x*n*(n+1) / 2
+    return int(sum)
+
+"""
+Returns sum of all natural numbers below x that are multiples of 3 or 5
+"""
+def sum_3_5(x):
+    total = arith_sum(x-1, 3) + arith_sum(x-1, 5) - arith_sum (x-1, 15)
+    return int(total)
+
+print(sum_3_5(1000))
---
+++ 003.py
@@ -0,0 +1,28 @@
+"""
+Project Euler Problem 3
+=======================
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143?
+"""
+import math
+
+def is_prime(x):
+    result = True
+    for i in range(2, int(math.sqrt(x))):
+        if x%i == 0:
+            result = False
+            break
+    return result
+
+def largest_prime_factor(x):
+    largest_factor = 0
+    for i in range(2, int(math.sqrt(x))):
+        if x%i == 0 and is_prime(i):
+            largest_factor = i
+    if largest_factor == 0:
+        largest_factor = x
+    return largest_factor
+
+print(largest_prime_factor(11))
