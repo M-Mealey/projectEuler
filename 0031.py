e932b6a6d324238e3a2bec0eb2aabe7ee51b0786
Megan Mealey <mealeymn@gmail.com>
2025-06-23 13:20:25 -0500

    problem 31

---
+++ 0031.py
@@ -0,0 +1,31 @@
+"""
+Project Euler Problem 31
+========================
+
+In England the currency is made up of pound, -L-, and pence, p, and there
+are eight coins in general circulation:
+
+  1p, 2p, 5p, 10p, 20p, 50p, -L-1 (100p) and -L-2 (200p).
+
+It is possible to make -L-2 in the following way:
+
+  1 * -L-1 + 1 * 50p + 2 * 20p + 1 * 5p + 1 * 2p + 3 * 1p
+
+How many different ways can -L-2 be made using any number of coins?
+"""
+
+p_amounts = [1, 2, 5, 10, 20, 50, 100, 200]
+
+def solve(coins, x):
+    if len(coins) == 1: # base case 2
+        return x%coins[0] == 0
+    next_coin = coins[-1]
+    remaining_coins = coins[:-1]
+    ways = 0
+    remaining_amt = x
+    while remaining_amt >= 0:
+        ways += solve(remaining_coins, remaining_amt)
+        remaining_amt -= next_coin
+    return ways
+
+print(solve(p_amounts, 200))
