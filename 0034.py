32f74b4978d6a72e15dfeae4872e53618d465a37
Megan Mealey <mealeymn@gmail.com>
2025-07-16 21:15:43 -0500

    problem 34

--- 0034.py
+++ 0034.py
@@ -30,3 +30,18 @@ Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 # This shows that there aren't any solutions where n>7, because the upper bound of values
 # for the sum of factorials of digits grows too slowly. Therefore, only numbers with 2 to 7
 # digits need to be checked
+
+import math
+
+# naive solution, check every number under 8 digits
+answer = 0
+for i in range(10, 10000000):
+    sum = 0
+    number = i
+    while number >0:
+        sum += math.factorial(number%10)
+        number = number//10
+    if sum==i:
+        answer += i
+
+print(answer)
