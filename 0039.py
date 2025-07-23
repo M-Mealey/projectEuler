82629a732386488770b01cc8a58b2725705583ad
Megan Mealey <mealeymn@gmail.com>
2025-07-23 17:35:43 -0500

    problem 39, lazy solution

---
+++ 0039.py
@@ -0,0 +1,24 @@
+"""
+Project Euler Problem 39
+========================
+
+If p is the perimeter of a right angle triangle with integral length
+sides, {a,b,c}, there are exactly three solutions for p = 120.
+
+                    {20,48,52}, {24,45,51}, {30,40,50}
+
+For which value of p < 1000, is the number of solutions maximised?
+"""
+
+max_solutions = (0, 0)
+for p in range(3, 1000):
+    solutions = 0
+    for a in range(1, p):
+        for b in range(a, p-a):
+            c = p - a - b
+            if a**2 + b**2 == c**2:
+                solutions += 1
+    if solutions > max_solutions[1]:
+        max_solutions = (p, solutions)
+
+print(max_solutions[0])
