5ec92b8f86fb1e6d0e1fb651415805db12f33921
Megan Mealey <mealeymn@gmail.com>
2025-08-28 10:10:26 -0500

    problem 49

---
+++ 0049.py
@@ -0,0 +1,60 @@
+"""
+Project Euler Problem 49
+========================
+
+The arithmetic sequence, 1487, 4817, 8147, in which each of the terms
+increases by 3330, is unusual in two ways: (i) each of the three terms are
+prime, and, (ii) each of the 4-digit numbers are permutations of one
+another.
+
+There are no arithmetic sequences made up of three 1-, 2-, or 3-digit
+primes, exhibiting this property, but there is one other 4-digit
+increasing sequence.
+
+What 12-digit number do you form by concatenating the three terms in this
+sequence?
+"""
+import itertools
+import math
+
+# find all combinations of 4 one-digit numbers
+# for each:
+#   if sum is multiple of 3, pass
+#   find permutations, eliminate any with 1st digit 0, last digit 0,2,4,5,6,8
+#   sort permutations (small to large), iterate over set
+
+def tuple_to_int(tup):
+    total = 0
+    for d in tup:
+        total = total * 10
+        total += d
+    return total
+
+def is_prime(x):
+    if not isinstance(x, int) or x<=1:
+        return False
+    for i in range(2, int(math.sqrt(x))+1):
+        if x%i == 0:
+            return False
+    return True
+
+# get all combinations of 4 digits
+digits = [0,1,2,3,4,5,6,7,8,9]
+combos = list(itertools.combinations_with_replacement(digits, 4))
+combos.remove((1,4,7,8)) # remove solution we already know
+
+
+for c in combos:
+    # get all prime numbers 4-digit numbers that can be made from these digits
+    numbers = set(filter(lambda x: x>1000 and is_prime(x), [tuple_to_int(p) for p in itertools.permutations(c)]))
+    # take one from set, get difference between each other element of set, and check for 3rd in arithmetic sequence
+    while len(numbers) > 2:
+        next_num = numbers.pop()
+        for n in numbers:
+            g, l = max(next_num, n), min(next_num,n)
+            diff = g - l
+            if g+diff in numbers:
+                print(str(l)+str(g)+str(g+diff))
+            if l-diff in numbers:
+                print(str(l-diff)+str(l)+str(g))
+
