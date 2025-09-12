6b585e5ab81e4fb72906ab95d499a7ce3e8a4e24
Megan Mealey <mealeymn@gmail.com>
2025-09-12 17:42:28 -0500

    problem 53

---
+++ 0053.py
@@ -0,0 +1,32 @@
+"""
+Project Euler Problem 53
+========================
+
+There are exactly ten ways of selecting three from five, 12345:
+
+           123, 124, 125, 134, 135, 145, 234, 235, 245, and 345
+
+In combinatorics, we use the notation, nCr(5,3) = 10.
+
+In general,
+
+nCr(n,r) = n!/(r!(n-r)!), where r =< n, n! = n * (n1) * ... * 3 * 2 * 1,
+and 0! = 1.
+
+It is not until n = 23, that a value exceeds one-million: nCr(23,10) =
+1144066.
+
+How many values of nCr(n,r), for 1 =< n =< 100, are greater than one-million?
+"""
+
+from math import comb
+
+count = 0
+
+for x in range(23, 101):
+    for y in range(1,x+1):
+        if comb(x, y) > 1000000:
+            count += 1
+
+print(count)
+
