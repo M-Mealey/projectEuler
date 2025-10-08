5cd80ef07ece726a3d90c5095fda4f8f8b47c76f
Megan Mealey <mealeymn@gmail.com>
2025-10-08 12:42:10 -0500

    problem 62 done correctly

--- 0062.py
+++ 0062.py
@@ -10,22 +10,19 @@ cube.
 Find the smallest cube for which exactly five permutations of its digits
 are cube.
 """
-import itertools
 
-cubes = [x**3 for x in range(300, 10000)]
+sorted_cubes = {}
+smallest_cube = {}
 
-def to_int(tup):
-    total = 0
-    for t in tup:
-        total *= 10
-        total += t
-    return total
-
-for c in cubes:
-    permutation_lists = list(itertools.permutations([int(ch) for ch in str(c)]))
-    permutations = set([to_int(p) for p in permutation_lists])
-    cube_permutations = [p for p in permutations if c <= p and p in cubes]
-    if len(cube_permutations) == 5:
-        print(c)
+for x in range(10, 10000):
+    c = x**3
+    # rearrange the digits in descending order. This forms a fingerprint that is the same for all permutations
+    digits_sorted = int("".join(sorted([ch for ch in str(c)], reverse=True)))
+    matches = sorted_cubes.get(digits_sorted, 0)
+    if matches == 0: # if there are no matching entries, this is the smallest cube with these digits.
+        smallest_cube[digits_sorted] = c
+    if matches == 4: # if this is the 5th match, search is over, return smallest cube for these digits
+        print(smallest_cube[digits_sorted])
         break
+    sorted_cubes[digits_sorted] = matches + 1
 
