6e738936de82d8988ead27a8c5c448f6e2f4c010
Megan Mealey <mealeymn@gmail.com>
2025-11-19 14:33:02 -0600

    problem 82

--- 0082.py
+++ 0082.py
@@ -17,12 +17,14 @@ moving up, down, and right, is indicated in red; the sum is equal to 994.
 Find the minimal path sum, in matrix.txt, a 31K text file containing a 80 by
 80 matrix, from the left column to the right column.
 """
+import bisect
+
 # shortest path to any square in left column = square itself
 # for each of the next column, create queue of path lengths, fill with path from left to each square
 #     the pop shortest path, add to shortest path matrix, add paths to each neighbor above/below,
 #     repeat until every square filled in column
 
-with open("resources/matrix.txt") as f:
+with open("resources/matrix_82.txt") as f:
     rows = f.read().strip().split("\n")
     data = [r.split(",") for r in rows]
     data = [[int(x) for x in r] for r in data]
@@ -32,15 +34,35 @@ with open("resources/matrix.txt") as f:
 #         path, a matrix the same size as costs for minimum path lengths to be written to
 def calculate_min_paths(costs, path):
     # first column
-    for r in range(len(data)):
-        path[r][0] = data[r][0]
-    # other rows
-    for c in range(1, len(data[0])):
-        continue
-        # queue
+    for r in range(len(costs)):
+        path[r][0] = costs[r][0]
+    # other columns
+    for c in range(1, len(costs[0])):
+        path_candidates = []
+        for r in range(len(costs)):
+            path_len = path[r][c-1] + costs[r][c]
+            path_candidates.append((path_len, r))
+        path_candidates.sort()
+        squares_filled = set()
+        while len(squares_filled) < len(costs) and len(path_candidates) > 0:
+            next_path = path_candidates.pop(0)
+            cost, row = next_path[0], next_path[1]
+            if row in squares_filled:
+                continue
+            path[row][c] = cost
+            squares_filled.add(row)
+            if row > 0: # add path to square above to candidates
+                path_up = (cost + costs[row-1][c], row-1)
+                bisect.insort(path_candidates, path_up)
+            if row < len(costs) - 1: # add path to square below to candidates
+                path_down = (cost + costs[row + 1][c], row + 1)
+                bisect.insort(path_candidates, path_down)
+
 
 paths = [[0 for _ in range(80)] for __ in range(80)]
 calculate_min_paths(data, paths)
-print(paths[79][79])
+min_path = min([r[79] for r in paths])
+print(min_path)
+
 
 
