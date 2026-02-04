cfb752dc7c7035dfdaea287818a893c512944f8d
Megan Mealey <mealeymn@gmail.com>
2026-02-04 13:42:13 -0600

    problem 105

---
+++ 101to200/105.py
@@ -0,0 +1,38 @@
+"""
+Project Euler Problem 105
+=========================
+
+Let S(A) represent the sum of elements in set A of size n. We shall call
+it a special sum set if for any two non-empty disjoint subsets, B and C,
+the following properties are true:
+
+ 1. S(B) != S(C); that is, sums of subsets cannot be equal.
+ 2. If B contains more elements than C then S(B) > S(C).
+
+For example, {81, 88, 75, 42, 87, 84, 86, 65} is not a special sum set
+because 65 + 87 + 88 = 75 + 81 + 84, whereas {157, 150, 164, 119, 79, 159,
+161, 139, 158} satisfies both rules for all possible subset pair
+combinations and S(A) = 1286.
+
+Using sets.txt, a 4K text file with one-hundred sets containing seven to
+twelve elements (the two examples given above are the first two sets in the
+file), identify all the special sum sets, A[1], A[2], ..., A[k], and find the
+value of S(A[1]) + S(A[2]) + ... + S(A[k]).
+
+NOTE: This problem is related to problems 103 and 106.
+"""
+from importlib import import_module
+# need to import like this because files are named numbers :)
+validate_set = import_module('103').validate_set
+
+with open("resources/sets.txt") as f:
+    data = f.read().split()
+
+total_sum = 0
+for set_str in data:
+    int_set = [int(num) for num in set_str.split(",")]
+    int_set.sort()
+    if validate_set(int_set):
+        total_sum += sum(int_set)
+print(total_sum)
+
