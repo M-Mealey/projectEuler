aa8e1b8abad0f03cb99a7a40da23a6e4edf7e65c
Megan Mealey <mealeymn@gmail.com>
2026-02-19 13:25:00 -0600

    still reading about pep8 :)

--- 1to100/001.py
+++ 1to100/001.py
@@ -8,23 +8,16 @@ we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
-"""
-Computes sum of arithmetic series from 0 to a, interval of x
-"""
-
 
 def arith_sum(a, x):
+    """Computes sum of arithmetic series from 0 to a, interval of x"""
     n = a // x
-    sum = x * n * (n + 1) / 2
-    return int(sum)
-
-
-"""
-Returns sum of all natural numbers below x that are multiples of 3 or 5
-"""
+    total = x * n * (n + 1) / 2
+    return int(total)
 
 
 def sum_3_5(x):
+    """Returns sum of all natural numbers below x that are multiples of 3 or 5"""
     total = arith_sum(x - 1, 3) + arith_sum(x - 1, 5) - arith_sum(x - 1, 15)
     return int(total)
 
