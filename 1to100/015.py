40d372fdc03eb6aad4eaf659350d19a83e0f6374
Megan Mealey <mealeymn@gmail.com>
2026-02-24 21:24:56 -0600

    testing math lib nCr vs. version I wrote

--- 1to100/015.py
+++ 1to100/015.py
@@ -7,14 +7,18 @@ Starting in the top left corner of a 2 * 2 grid, there are 6 routes
 
 How many routes are there through a 20 * 20 grid?
 """
-
+import math
 from math import factorial
+import time
 
 """
 Computes nCr for given n and r
 """
-def ncr(n,r):
+
+
+def ncr(n, r):
     return int(factorial(n) / (factorial(r) * factorial(n-r)))
+
 
 """
 Each step is either in the +x or +y direction, 
@@ -25,8 +29,23 @@ It's equivalent to figuring out how many ways you can flip a coin 40 times and e
 20 heads and 20 tails
 Mathematically, this can be calculated as nCr, where n=40 and r=20
 """
-def euler_problem_15():
-    print(ncr(40,20))
+
+
+def euler_problem_15(compare=False):
+    start_time = time.perf_counter()
+    print(math.comb(40, 20))
+    end_time = time.perf_counter()
+    elapsed_time = end_time - start_time
+    if compare:
+        print(f"solve with math lib took {elapsed_time} seconds")
+
+        start_time = time.perf_counter()
+        print(ncr(40, 20))
+        end_time = time.perf_counter()
+        elapsed_time = end_time - start_time
+        print(f"solve with written method took {elapsed_time} seconds")
+
 
 if __name__ == "__main__":
-    euler_problem_15()
+    compare_solutions = True
+    euler_problem_15(compare_solutions)
