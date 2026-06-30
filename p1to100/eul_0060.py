45090fcdc3d7de2a3ddfaaeaac50f77a3cd48bce
Megan Mealey <mealeymn@gmail.com>
2026-06-30 10:12:31 -0500

    problem 60 under 7 seconds

--- p1to100/eul_0060.py
+++ p1to100/eul_0060.py
@@ -17,57 +17,54 @@ try:
 except ModuleNotFoundError:
     from local_helpers import miller_rabin_prime_test, prime_sieve
 
-MAX_PRIME = 20000
-primes = prime_sieve(MAX_PRIME)
+MAX_PRIME = 2000000
+prime_list = prime_sieve(MAX_PRIME)
+primes = set(prime_list)
 primes.remove(5)  # only prime that ends in 5 is 5
 
 
 def check_prime_pair(x, y):
     """ check whether two primes concatenate to form another prime """
-    return miller_rabin_prime_test(int(str(x)+str(y))) and miller_rabin_prime_test(int(str(y)+str(x)))
+    p1, p2 = int(f"{x}{y}"), int(f"{y}{x}")
+    if p1 in primes and p2 in primes:
+        return True
+    return miller_rabin_prime_test(p1) and miller_rabin_prime_test(p2)
+
+
+def verify_set(p_l, p_lookup):
+    """ verify that numbers in p_list satisfy the problem """
+    for p1, p2 in combinations(p_l, 2):
+        if p1 not in p_lookup[p2] or p2 not in p_lookup[p1]:
+            return False
+    return True
 
 
 def solve():
     """ solve problem 60 """
-    all_prime_pairs = list(combinations(primes, 2))
-    print(all_prime_pairs)
-    prime_pair_lookup = {x:[] for x in primes}
-    print(prime_pair_lookup)
-    for p1, p2 in all_prime_pairs:
-        if check_prime_pair(p1, p2):
-            prime_pair_lookup[p1].append(p2)
-    for k in prime_pair_lookup:
-        if len(prime_pair_lookup[k]) >= 4:
-            print(k)
-            print(prime_pair_lookup[k])
 
+    visited_primes = set()
+    prime_pair_lookup = {}
+    pairs_found = {}
+    for p in prime_list:
+        prime_pair_lookup[p] = set()
+        pairs_found[p] = 0
+        for v in visited_primes:
+            if check_prime_pair(p, v):
+                prime_pair_lookup[v].add(p)
+                prime_pair_lookup[p].add(v)
+                pairs_found[p] += 1
+                pairs_found[v] += 1
+        visited_primes.add(p)
+        if pairs_found[p] >= 4:
+            pair_list = [x for x in prime_pair_lookup[p] if pairs_found[x] >= 4 and len(
+                prime_pair_lookup[x].intersection(prime_pair_lookup[p])) >= 3]
+            if len(pair_list) >= 4:
+                for p_combo in combinations(pair_list, 4):
+                    p_list = list(p_combo)
+                    p_list.append(p)
+                    if verify_set(p_list, prime_pair_lookup):
+                        return sum(p_list)
     return -1
-
-    p_sum = 200000
-    while len(primes) > 0:
-        p = primes.pop(0)
-        if p * 5 > p_sum:
-            continue
-        pairs = [x for x in primes if check_prime_pair(p, x)]
-        while len(pairs) > 0:
-            pj = pairs.pop(0)
-            if p + pj * 4 > p_sum:
-                continue
-            triples = [x for x in pairs if check_prime_pair(pj, x)]
-            while len(triples) > 0:
-                pt = triples.pop(0)
-                if p + pj + pt * 3 > p_sum:
-                    continue
-                quads = [x for x in triples if check_prime_pair(pt, x)]
-                while len(quads) > 0:
-                    pq = quads.pop(0)
-                    if p + pj + pt + pq * 2 > p_sum:
-                        continue
-                    quints = [x for x in quads if check_prime_pair(pq, x)]
-                    if len(quints) > 0:
-                        p_sum = min(p_sum, p + pj + pt + pq + quints[0])
-
-    return p_sum
 
 
 if __name__ == "__main__":
