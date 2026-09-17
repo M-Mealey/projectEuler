320235aa28739fd30b0dd34d3fc78f2215610d3e
Megan Mealey <mealeymn@gmail.com>
2026-09-17 12:34:57 -0500

    problem 114

--- p101to200/eul_0114.py
+++ p101to200/eul_0114.py
@@ -32,4 +32,20 @@ measuring eight units in length you could use red (3), black (1), and red
 # dynamic programming problem:
 # if first square is grey, then number of ways to make 50 is num ways to make 49
 # if first square is red, then it's part of red bar followed by a grey
-# numberf of ways is 50 - (len(red bar) + 1)
+# number of ways is 50 - (len(red bar) + 1)
+
+def solve():
+    """ solve problem 114 """
+    ways = {0: 1, 1: 1, 2: 1, 3: 2, 4: 4, 5:7, 6:11}
+    n = 7
+    while n<51:
+        ways_for_n = ways[n-1] # first block grey
+        ways_for_n += 1 # one big red block
+        for r in range(4, n+1): # first block red, len r-1, + next block grey
+            ways_for_n += ways[n-r]
+        ways[n] = ways_for_n
+        n += 1
+    return ways[50]
+
+if __name__ == "__main__":
+    print(solve())
